4ec9210246441e8166b7701f87c6c772f73a697f
Akshit Choudhary <akshitchoudhary007@gmail.com>
2023-05-10 00:47:16 +0530

    Create ERC20Mock.sol

---
+++ contracts/ERC20Mock.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract ERC20Mock is ERC20 {
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
+
+    function mint(address account, uint256 amount) external {
+        _mint(account, amount);
+    }
+
+    function burn(address account, uint256 amount) external {
+        _burn(account, amount);
+    }
+}
